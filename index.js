5972d41b6197f684dbfdb07c7f1329b843d04227
Tyler Lampel <lampel13@gmail.com>
2022-04-28 21:02:25 -0400

    commit tree c3f958558c4f2e397ba4c2038dbe242bf7c7522c

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// code your solution here
+function saturdayFun(activity) {
+    activity= (typeof activity !== 'undefined') ? activity : "roller-skate";
+    return `This Saturday, I want to ${activity}!`;
+}
+function mondayWork(work) {
+    work= (typeof work !== 'undefined') ? work : "go to the office";
+    return `This Monday, I will ${work}.`;
+}
+function wrapAdjective(adj){
+    return function (spec) {
+        spec= (typeof spec !== 'undefined' ? spec : 'special')
+        return `You are ${adj}${spec}${adj}!`
+    }
+}
